81c3542580d166b72152b9169dfa679d11416b62
Viggo Gascou <contact@viggogascou.co>
2026-04-10 13:40:04 +0200

    fix: actually forgot the combo modes

--- submit.sh
+++ submit.sh
@@ -15,8 +15,8 @@ mkdir -p logs/SLURM
 REPO_ROOT="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 submit_job() {
-    # if mode sync or sync-train git pull
-    if [[ "$MODE" == "sync" || "$MODE" == "sync-train" ]]; then
+    # if mode sync or sync-train or sync-run git pull
+    if [[ "$MODE" == "sync" || "$MODE" == "sync-train" || "$MODE" == "sync-run" ]]; then
         git pull
     fi
     LAST_JOB_ID=$(sbatch --parsable --chdir="$REPO_ROOT" "$@")
